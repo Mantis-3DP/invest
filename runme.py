d0687b38d01c8be7b196823d345baa8c040e136b
Mantis-3DP <mantis3dp@posteo.de>
2021-01-17 00:14:46 +0100

    removed qm_aprt from calcNetRent input

--- runme.py
+++ runme.py
@@ -6,15 +6,14 @@
 #
 # print(data)
 
-import csv
+
 import numpy as np
-
-
 import pandas as pd
 import pathlib
 
 datapath = pathlib.Path(__file__).parent
-roomPrices = pd.read_csv(str(datapath)+"/data/qmPrices.csv", index_col=0)
+roomPrices = pd.read_csv(str(datapath) + "/data/qmPrices.csv", index_col=0)
+
 
 class investment:
 
@@ -25,17 +24,16 @@ class investment:
         self.GFZ = GFZ
         self.wertHaus = wertHaus
 
-
         pass
 
-    def calcNetRent(self, N_aprt, Qm_aprt, count_room):
+    def calcNetRent(self, N_aprt, count_room):
 
         # N_aprt: total number of apartments in the building
         # Qm_aprt: Size of a single of the apartments
         # count_room: number of rooms in each apartment
         # qmPrices: price per qm in dependence of number of rooms in that apartment
 
-        #dictionarys for the different amount of rooms
+        # dictionaries for the different amount of rooms
 
         datapath = pathlib.Path(__file__).parent
         qmPrices = pd.read_csv(str(datapath) + "/data/qmPrices.csv", index_col=0)
@@ -45,15 +43,18 @@ class investment:
         if count_room > 5:
             count_room = 5
 
-        #calculation of the qm price
-        if Qm_aprt%5 == 0:
+        # calculation of the qm price
+        Qm_aprt = self.aWohn() / N_aprt
+
+        if Qm_aprt % 5 == 0:
             return round((qmPrices[count_room][Qm_aprt] * Qm_aprt) * N_aprt, 2)
 
         else:
-        	# with linear interpolation
-            Qm_aprt_lower = Qm_aprt - Qm_aprt%5
+            # with linear interpolation
+            Qm_aprt_lower = Qm_aprt - Qm_aprt % 5
             Qm_aprt_upper = Qm_aprt_lower + 5
-            interpolation = ((qmPrices[count_room][Qm_aprt_upper] - qmPrices[count_room][Qm_aprt_lower]) / 5) * (Qm_aprt%5)
+            interpolation = ((qmPrices[count_room][Qm_aprt_upper] - qmPrices[count_room][Qm_aprt_lower]) / 5) * (
+                        Qm_aprt % 5)
 
             return round(((qmPrices[count_room][Qm_aprt_lower] + interpolation) * Qm_aprt) * N_aprt, 2)
 
@@ -63,12 +64,12 @@ class investment:
 
     def ruecklagen(self):
         rueckpa = self.wertHaus * 0.02
-        rueckpm = rueckpa/12
+        rueckpm = rueckpa / 12
 
         return rueckpa, rueckpm
 
-if __name__ == "__main__":
 
-    test = investment(4,75,0.4,700000)
-    print(test.aWohn())
-    print(test.calcNetRent(N_aprt = 6, Qm_aprt = 74.53, count_room = 3))
+if __name__ == "__main__":
+    test = investment(4, 564, 0.8, 700000)
+    print("Monatliche Nettomieteinnahmen sind: {}€"
+          .format(test.calcNetRent(N_aprt=6, count_room=3)))
