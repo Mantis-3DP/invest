51c4254ba571bf3050e1f9f7a6d57fc64dd9f854
Mantis-3DP <mantis3dp@posteo.de>
2021-01-26 22:43:25 +0100

    rename file and class to building

---
+++ tobuy.py
@@ -0,0 +1,12 @@
+# this should calculate what you should buy to reach a desired portfolio.
+# as an input you need:
+## what you have
+## desired allocotion in portfolio
+## sum you want to invest
+## min. investment per buy
+
+# what the program should do:
+## load current prices
+## load price historie
+
+## should be able to shift an investment into the future to reach the min. investment per buy
